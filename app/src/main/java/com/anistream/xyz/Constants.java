93a0c88adc0ec93d2cc68a824bc0c365f2b977e6
Delta <riku@kawaii.sh>
2020-08-27 09:00:15 -0500

    prerelease

--- app/src/main/java/com/anistream/xyz/Constants.java
+++ app/src/main/java/com/anistream/xyz/Constants.java
@@ -3,5 +3,5 @@ package com.anistream.xyz;
 public class Constants {
     //static String url = "https://3029825460.cobaltmedia.xyz/";
     static String url = "https://www2.gogoanime.video/";
-    public static final int APP_VER = 11;
+    public static final int APP_VER = 12;
 }
